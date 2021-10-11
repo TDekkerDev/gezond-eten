86df2b17d45f7aa144ee36ff7f9760baa5a8bd95
ThomasSit <89969304+ThomasSit@users.noreply.github.com>
2021-10-11 12:19:27 +0200

    all

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bmi</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div class="wrapper">
+    <h2> Bmi test </h2>
+    <form action="" method="post">    
+<Div class=" section1">
+        <span> enter here your weight: </span>
+        <input type="number" name="weight"
+        autocomplete="off" placeholder="miter">
+</div>
+<Div class=" section2">
+        <span> enter here your height: </span>
+        <input type="number" name="height"
+        autocomplete="off" placeholder="miter">
+</div>
+<Div class=" submit">
+        <input type="submit" name="submit"
+        value="check bmi">
+<input type="reset" value="clear">
+
+       
+</div>
+
+    
+
+
+</form>
+
+
+
+
+
+
+
+</body>
+</html>
