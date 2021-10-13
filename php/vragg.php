28bb26d4b21fa123b26beb79af1713887125cdaa
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2021-10-13 09:57:31 +0200

    ww

---
+++ php/vragg.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+    <body>
+        
+        <div class="vraag">
+                <span> Is er voldoende gezond aanbod in de kantine op school?
+                    <select name="dag" id="vraag_1">
+                        <option value="zaterdag">ja</option>
+                        <option value="zondag">nee</option>
+                    </select>
+                </span>
+
+                <span> Hoe vaak drink je frisdrank?
+                    <select name="dag" id="vraag_2">
+                        <option value="zaterdag">Elke dag</option>
+                        <option value="zondag">Paar x per week</option>
+                        <option value="zondag">Bijna nooit</option>
+                    </select>
+                </span>
+
+                <span> Hoe vaak eet je fruit?
+                    <select name="dag" id="vraag_3">
+                        <option value="zaterdag">Elke dag</option>
+                        <option value="zondag">Paar x per week</option>
+                        <option value="zondag">Bijna nooit</option>
+                    </select>
+                </span>
+
+
+                <span> Hoe vaak ontbijt je?
+                    <select name="dag" id="vraag_4">
+                        <option value="zaterdag">Elke dag</option>
+                        <option value="zondag">Paar x per week</option>
+                        <option value="zondag">Bijna nooit</option>
+                    </select>
+                </span>
+
+
+
+                <span> Eet je wel eens na 11 uur ‘s avonds
+                    <select name="dag" id="vraag_5">
+                        <option value="zaterdag">Vaak</option>
+                        <option value="zondag">Nooit</option>
+                        <option value="zondag">Altijd</option>
+                    </select>
+                </span>
+
+
+
+                <span>Wat is je BMI?
+                    <select name="dag" id="vraag_6">
+                        <option value="zaterdag">Ondergewicht</option>
+                        <option value="zondag">Goed</option>
+                        <option value="zondag">Overgewicht</option>
+                    </select>
+                </span>
+
+        </div>
+
+
+
+
+
+
+
+
+    </body>
+</html>
