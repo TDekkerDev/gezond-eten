281fa4251d30f64d19d2aa4f01d3fcaa22c276d2
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2021-10-14 10:07:22 +0200

    werk

---
+++ php/navbar.php
@@ -0,0 +1,9 @@
+<header>
+            <img class="logo" src="../img/beter.png" alt="logo">
+            <ul class="nav">
+                <li><a href="home.php" class="active">Home</a></li>
+                <li><a href="holy.php">About</a></li>
+                <li><a href="#">Services</a></li>
+            </ul>
+            <a class="button" href="#"><button>Contact</button></a>
+        </header>
