09bbeb88762a1af862e865ca251715f9ccab92d8
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2021-10-26 12:12:02 +0200

    gg

--- php/navbar.php
+++ php/navbar.php
@@ -1,9 +1,12 @@
 <header>
-            <img class="logo" src="../img/beter.png" alt="logo">
+            <img class="logo" src="img/beter.png" alt="logo">
             <ul class="nav">
                 <li><a href="home.php" class="active">Home</a></li>
-                <li><a href="holy.php">About</a></li>
-                <li><a href="#">Services</a></li>
+                <li><a href="#">informatie</a></li>
+                <li><a href="#">games</a></li>
+                <li><a href="#">voedsel</a></li>
+                <li><a href="php/vragg.php">vragenlijft</a></li>
+
             </ul>
-            <a class="button" href="#"><button>Contact</button></a>
+            <a class="button" href="contact.php"><button>Contact</button></a>
         </header>
